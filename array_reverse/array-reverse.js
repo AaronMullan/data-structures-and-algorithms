810d38525d450e0e4aeb022fcd2d82aab8957626
AaronMullan <aaroncmullan@gmail.com>
2019-12-02 22:02:27 -0800

    Tavisloversdotcom

--- array_reverse/array-reverse.js
+++ array_reverse/array-reverse.js
@@ -2,10 +2,10 @@ const arrOne = [1, 2, 3, 4, 5, 6];
 
 const arrayReverser = arr => {
   let reversedArray = [];
-  for(let i = arr.length -1; i >= 0; i--) {
-      reversedArray.push(arr[i]);
-      }
+  for(let i = arr.length - 1; i >= 0; i--) {
+    reversedArray.push(arr[i]);
+    }
   return reversedArray;
-  };
+};
 const backwardsArray = arrayReverser(arrOne);
 console.log(backwardsArray);
