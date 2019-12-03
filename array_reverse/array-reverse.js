45221d8b1b30e934b594e4e3a804b4e72c30c3ce
AaronMullan <aaroncmullan@gmail.com>
2019-12-02 21:56:57 -0800

    Travis hope you like spaces

--- array_reverse/array-reverse.js
+++ array_reverse/array-reverse.js
@@ -1,11 +1,11 @@
 const arrOne = [1, 2, 3, 4, 5, 6];
 
 const arrayReverser = arr => {
-	let reversedArray = [];
-	for(let i = arr.length -1; i >= 0; i--) {
-			reversedArray.push(arr[i]);
-			}
-	return reversedArray;
-	};
+  let reversedArray = [];
+  for(let i = arr.length -1; i >= 0; i--) {
+      reversedArray.push(arr[i]);
+      }
+  return reversedArray;
+  };
 const backwardsArray = arrayReverser(arrOne);
 console.log(backwardsArray);
