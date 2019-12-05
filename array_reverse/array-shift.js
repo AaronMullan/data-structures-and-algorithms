2bd934b57647c6b31261c73fe5c8f4cf271f51c8
AaronMullan <aaroncmullan@gmail.com>
2019-12-05 12:45:35 -0800

    fake binary search working

--- array_reverse/array-shift.js
+++ array_reverse/array-shift.js
@@ -1,6 +1,3 @@
-const arr = [1, 2, 3, 4, 5];
-const n = 'hi';
-
 const arrayShifter = (n, arr) => {
   let arrayShifted = arr;
   const halfway = arr.length / 2;
@@ -8,7 +5,5 @@ const arrayShifter = (n, arr) => {
   return arrayShifted;
 };
 
-const badabing = arrayShifter(n, arr);
-console.log(badabing);
 
 module.exports = { arrayShifter };
