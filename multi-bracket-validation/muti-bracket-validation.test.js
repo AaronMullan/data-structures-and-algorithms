c706429af26ee97a1863d5c21c96480733f362e0
AaronMullan <aaroncmullan@gmail.com>
2019-12-18 21:37:21 -0800

    first validator commit

---
+++ multi-bracket-validation/muti-bracket-validation.test.js
@@ -0,0 +1,8 @@
+const validator = require('./multi-bracket-validation');
+describe('CueWithStacks methods', () => {
+  
+    let queue = [10, 15, 20];
+  it('has an encue method', () => {
+    const cue = new PseudoQueue();
+
+  });
