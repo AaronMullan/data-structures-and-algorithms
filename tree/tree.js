05958239f8311bb1da7da7f041cc1819ee12c2c7
AaronMullan <aaroncmullan@gmail.com>
2020-01-29 08:55:08 -0800

    binary tree

---
+++ tree/tree.js
@@ -0,0 +1,36 @@
+class Node {
+  constructor(val) {
+    this.value = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinaryTree {
+  constructor(){
+    this.root = null;
+  }
+  perorder(){
+
+  }
+
+  inorder(){
+
+  }
+
+  postorder(){
+
+  }
+}
+
+class BinarySearchTree {
+  constructor(){
+
+  }
+  add(){
+
+  }
+  contains(){
+
+  }
+};
