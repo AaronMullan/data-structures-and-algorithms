ac32b16a8ac09b6e84c9c489da525dd7222bf20f
AaronMullan <aaroncmullan@gmail.com>
2020-01-30 08:48:00 -0800

    fizzBuzzTree

--- fizzBuzzTree/fizzBuzzTree.js
+++ fizzBuzzTree/fizzBuzzTree.js
@@ -8,28 +8,28 @@ function Tree(data) {
   this._root = node;
 }
 
-const tree = new Tree('one');
+const tree = new Tree(3);
  
-tree._root.children.push(new Node('two'));
+tree._root.children.push(new Node(5));
 tree._root.children[0].parent = tree;
  
-tree._root.children.push(new Node('three'));
+tree._root.children.push(new Node(15));
 tree._root.children[1].parent = tree;
  
-tree._root.children.push(new Node('four'));
+tree._root.children.push(new Node(4));
 tree._root.children[2].parent = tree;
  
-tree._root.children[0].children.push(new Node('five'));
+tree._root.children[0].children.push(new Node(17));
 tree._root.children[0].children[0].parent = tree._root.children[0];
  
-tree._root.children[0].children.push(new Node('six'));
+tree._root.children[0].children.push(new Node(60));
 tree._root.children[0].children[1].parent = tree._root.children[0];
  
-tree._root.children[2].children.push(new Node('seven'));
+tree._root.children[2].children.push(new Node(25));
 tree._root.children[2].children[0].parent = tree._root.children[2];
 
 console.log(tree);
 
-// function Fizzbuzztree (tree){
-//   //now some magic happens
-// }
+function Fizzbuzztree (tree){
+  //now some magic happens
+}
